603394ba4a17061cdda832585d7f7e02aeadbe25
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2021-01-07 17:51:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+//Create variables here
+
+var dogImg, happyDogImg
+var foodStock
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage("Dog.png");
+  happyDogImg = loadImage("happydog.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  foodStock=database.ref('food');
+  foodStock.on("value", readStock);
+  
+}
+
+
+function draw() {  
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDogImg);
+
+  }
+  drawSprites();
+  //add styles here
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+  
+
+}
+
+
+
